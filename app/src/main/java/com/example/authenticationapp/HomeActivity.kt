22173cb22787e3a5216bf985e4a08e7cda9b553a
Mahadev008 <mahadevan.periasamy001@gmail.com>
2023-04-08 23:27:01 +0530

    First Commit!

---
+++ app/src/main/java/com/example/authenticationapp/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.example.authenticationapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.authenticationapp.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//  Auth
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnLogout.setOnClickListener {
+            auth.signOut()
+            Intent(this, LoginActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(it)
+                Toast.makeText(this, "Logout Successfully!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
